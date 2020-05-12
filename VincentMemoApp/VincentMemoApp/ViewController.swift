5d0ed8358ef73ae34b315cf706b12f02acc73632
김광준 <kwangjun3952@gmail.com>
2020-05-12 20:42:00 +0900

    first init

---
+++ VincentMemoApp/VincentMemoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VincentMemoApp
+//
+//  Created by 김광준 on 2020/05/12.
+//  Copyright © 2020 VincentGeranium. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
